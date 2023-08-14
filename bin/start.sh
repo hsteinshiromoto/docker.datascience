1b0380c94a165d51dcdd2064da8f4748ef7a7d9b
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2023-08-14 22:38:59 +1000

    remove need for password in jupyter lab

--- bin/start.sh
+++ bin/start.sh
@@ -5,4 +5,4 @@ code-server --port 8080 --bind-addr 0.0.0.0 &
 
 
 echo "Starting Jupyter Lab"
-jupyter lab --port=8888 --no-browser --ip=0.0.0.0 --allow-root
+jupyter lab --port=8888 --no-browser --ip=0.0.0.0 --allow-root --NotebookApp.token='' --NotebookApp.password=''
